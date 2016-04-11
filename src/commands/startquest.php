2ae60a6c478abfcb60bb3eb545775a15f7377ccd
Blabla <st.ten.veldhuis@gmail.com>
2016-04-11 21:42:54 +0200

    Update startquest.php

--- src/commands/startquest.php
+++ src/commands/startquest.php
@@ -17,14 +17,25 @@ class startquest extends PluginBase {
             if($p->getExpLevel()) == 0) {
               $p->sendMessage(TF::GRAY . "Quest started: Helping the Farmer");
               $p->sendMessage(TF::AQUA . "Collect 5 pieces of wheat.");
+              $p->givePermission("quest.1.complete");
             }
             break;
           
           case "2":
             if($p->getExpLevel()) == 1) {
-              $p->sendMessage(TF::GRAY . "Quest started: ...");
-              $p sendMessage(TF::AQUA . "...")
+              $p->sendMessage(TF::GRAY . "Quest started: Not enough Trees");
+              $p sendMessage(TF::AQUA . "Collect 16 Oak Saplings")
+              $p->givePermission("quest.2.complete");
             }
+            break;
+            
+          case "3":
+            if($p->getExpLevel()) == 2) {
+              $p->sendMessage(TF::GRAY > "Quest started: Searching the Stick");
+              $p->sendMessage(TF::AQUA . "Collect 1 stick");
+              $p->givePermission("quest.3.complete");
+            }
+            break;
         }
     }
   }
