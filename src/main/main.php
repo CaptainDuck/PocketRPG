0b726798128fe2598acf970fdb5281e334e63db4
Blabla <st.ten.veldhuis@gmail.com>
2016-04-11 11:05:53 +0200

    Update main.php

--- src/main/main.php
+++ src/main/main.php
@@ -1,1 +1,21 @@
+<?php
 
+namespace Sandertv\Quests
+
+use pocketmine\
+
+class listquest extends PluginBase implements Listener {
+  
+  public function onLoad() {
+    $this->getLogger()->info("Quests loading!");
+  }
+  
+  public function onEnable() {
+    $this->getLogger()->info("Enabling Quests!");
+      @mkdir($this->getDataFolder());
+      $this->config = new Config ($this->getDataFolder() . "config.yml" , Config::YAML, array());
+  }
+  
+  public function onDisable() {
+    $this->getLogger()->info("Disabling Quests!");
+  }
