7f5b710f4763c3617fda55172ef35cad3256f0ef
Blabla <st.ten.veldhuis@gmail.com>
2016-04-11 10:59:42 +0200

    Update and rename src/main.php to src/commands/listquest.php

---
+++ src/commands/listquest.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Sandertv\Quests;
+
+use pocketmine\plugin\pluginbase;
+use pocketmine\utils\config;
+use pocketmine\utils\TextFormat as TF;
+use pocketmine\event\listener;
+use pocketmine\command\command;
+use pocketmine\command\commandSender;
+use pocketmine\command\commandExecutor;
+use pocketmine\item\item;
+
+class listquest extends PluginBase implements Listener {
+  
+  public function onLoad() {
+    $this->getLogger()->info("Quests loading!");
+  }
+  
+  public function onEnable() {
+    $this->getLogger()->info("Enabling Quests!");
+      @mkdir($this->getDataFolder());
+      $this->config = new Config ($this->getDataFolder() . "config.yml" , Config::YAML, array());
+  }
+  
+  public function onDisable() {
+    $this->getLogger()->info("Disabling Quests!");
+  }
+  
+  public function onCommand(CommandSender $p, Command $cmd, $label, array $args) {
+    if($cmd->getName() == "listquest") {
+      if(!isset($args[0])) {
+        $p->sendMessage(TF::BOLD . "Quests:");
+          if($p->hasPermission("quest") || $p->hasPermission("quest.1") || $p->hasPermission("quest.1.completed") || $p->getExperience() == 0 {
+              $p->sendMessage(TF::GREEN . "Helping the Farmer - lvl. 0");
+          } elseif($p->getExperience()) == 0) {
+              $p->sendMessage(TF::YELLOW . "Helping the Farmer - lvl. 0");
+          } else {
+              $p->sendMessage(TF::RED . "Helping the Farmer - lvl. 0");
+          }
+
+          if($p->hasPermission("quest") || $p->hasPermission("quest.2") || $p->hasPermission("quest.2.completed") {
+              $p->sendMessage(TF::GREEN . "Not enough Trees - lvl. 1");
+          } elseif($p->getExperience()) == 1) {
+              $p->sendMessage(TF::YELLOW . "Not enough Trees - lvl. 1");
+          } else {
+              $p->sendMessage(TF::RED . "Not enough Trees - lvl. 1");
+          }
