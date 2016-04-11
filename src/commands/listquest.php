e46ab7652315a88345e0c195127176061e79af62
Blabla <st.ten.veldhuis@gmail.com>
2016-04-11 21:38:27 +0200

    Update listquest.php

--- src/commands/listquest.php
+++ src/commands/listquest.php
@@ -20,54 +20,54 @@ class listquest extends PluginBase implements Listener {
           case "1":
             $p->sendMessage(TF::BOLD . DARK_GREEN . "Quests page 1:");
               if($p->hasPermission("quest") || $p->hasPermission("quest.1") || $p->hasPermission("quest.1.completed") || $p->getExpLevel() == 0 {
-                  $p->sendMessage(TF::GREEN . "Helping the Farmer - lvl. 0");
+                  $p->sendMessage(TF::GREEN . "1 - Helping the Farmer - lvl. 0");
               } elseif($p->getExpLevel()) == 0) {
-                  $p->sendMessage(TF::YELLOW . "Helping the Farmer - lvl. 0");
+                  $p->sendMessage(TF::YELLOW . "1 - Helping the Farmer - lvl. 0");
               } else {
-                  $p->sendMessage(TF::RED . "Helping the Farmer - lvl. 0");
+                  $p->sendMessage(TF::RED . "1 - Helping the Farmer - lvl. 0");
               }
 
               if($p->hasPermission("quest") || $p->hasPermission("quest.2") || $p->hasPermission("quest.2.completed") || $p->getExpLevel() == 1 {
-                  $p->sendMessage(TF::GREEN . "Not enough Trees - lvl. 1");
+                  $p->sendMessage(TF::GREEN . "2 - Not enough Trees - lvl. 1");
               } elseif($p->getExpLevel()) == 1) {
-                  $p->sendMessage(TF::YELLOW . "Not enough Trees - lvl. 1");
+                  $p->sendMessage(TF::YELLOW . "2 - Not enough Trees - lvl. 1");
               } else {
-                  $p->sendMessage(TF::RED . "Not enough Trees - lvl. 1");
+                  $p->sendMessage(TF::RED . "2 - Not enough Trees - lvl. 1");
               }
           
               if($p->hasPermission("quest") || $p->hasPermission("quest.3") || $p->hasPermission("quest.3.completed") || $p->getExpLevel() == 2 {
-                  $p->sendMessage(TF::GREEN . "Searching the Stick - lvl. 2");
+                  $p->sendMessage(TF::GREEN . "3 - Searching the Stick - lvl. 2");
               } elseif($p->getExpLevel()) == 2) {
-                  $p->sendMessage(TF::YELLOW . "Searching the Stick - lvl. 2");
+                  $p->sendMessage(TF::YELLOW . "3 - Searching the Stick - lvl. 2");
               } else {
-                  $p->sendMessage(TF::RED . "Searching the Stick - lvl. 2");
+                  $p->sendMessage(TF::RED . "3 - Searching the Stick - lvl. 2");
               }
           
               if($p->hasPermission("quest") || $p->hasPermission("quest.4") || $p->hasPermission("quest.4.completed") || $p->getExpLevel() == 3 {
-                  $p->sendMessage(TF::GREEN . "Filling the Well - lvl. 3");
+                  $p->sendMessage(TF::GREEN . "4 - Filling the Well - lvl. 3");
               } elseif($p->getExpLevel()) == 3) {
-                  $p->sendMessage(TF::YELLOW . "Filling the Well - lvl. 3");
+                  $p->sendMessage(TF::YELLOW . "4 - Filling the Well - lvl. 3");
               } else {
-                  $p->sendMessage(TF::RED . "Filling the Well - lvl. 3");
+                  $p->sendMessage(TF::RED . "4 - Filling the Well - lvl. 3");
               }
 
               if($p->hasPermission("quest") || $p->hasPermission("quest.5") || $p->hasPermission("quest.5.completed") || $p->getExpLevel() == 4 {
-                  $p->sendMessage(TF::GREEN . "The ancient Gem - lvl. 4");
+                  $p->sendMessage(TF::GREEN . "5 - The ancient Gem - lvl. 4");
               } elseif($p->getExpLevel()) == 4) {
-                  $p->sendMessage(TF::YELLOW . "The ancient Gem - lvl. 4");
+                  $p->sendMessage(TF::YELLOW . "5 - The ancient Gem - lvl. 4");
               } else {
-                  $p->sendMessage(TF::RED . "The ancient Gem - lvl. 4");
+                  $p->sendMessage(TF::RED . "5 - The ancient Gem - lvl. 4");
               }
             break;
           
           case "2":
             $p->sendMessage(TF::BOLD . DARK_GREEN . "Quests page 2:");
               if($p->hasPermission("quest") || $p->hasPermission("quest.6") || $p->hasPermission("quest.6.completed") || $p->getExpLevel() == 5 {
-                  $p->sendMessage(TF::GREEN . "Freezingly cold - lvl. 5");
+                  $p->sendMessage(TF::GREEN . "6 - Freezingly cold - lvl. 5");
               } elseif($p->getExpLevel()) == 5) {
-                  $p->sendMessage(TF::YELLOW . "Freezingly cold - lvl. 5");
+                  $p->sendMessage(TF::YELLOW . "6 - Freezingly cold - lvl. 5");
               } else {
-                  $p->sendMessage(TF::RED . "Freezingly cold - lvl. 5");
+                  $p->sendMessage(TF::RED . "6 - Freezingly cold - lvl. 5");
               }
             break;
             
