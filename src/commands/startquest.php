02fa9d3051cc16e3f371ba70c754b85c7850a15d
Blabla <st.ten.veldhuis@gmail.com>
2016-04-11 21:36:21 +0200

    Update startquest.php

--- src/commands/startquest.php
+++ src/commands/startquest.php
@@ -10,6 +10,22 @@ use pocketmine\command\CommandSender;
 class startquest extends PluginBase {
   
   public function onCommand(CommandSender $p, Command $cmd, $label, array $args) {
-    
+    switch($cmd->getName()) {
+      case "startquest":
+        switch(strtolower(array_shift($args))) {
+          case "1":
+            if($p->getExpLevel()) == 0) {
+              $p->sendMessage(TF::GRAY . "Quest started: Helping the Farmer");
+              $p->sendMessage(TF::AQUA . "Collect 5 pieces of wheat.");
+            }
+            break;
+          
+          case "2":
+            if($p->getExpLevel()) == 1) {
+              $p->sendMessage(TF::GRAY . "Quest started: ...");
+              $p sendMessage(TF::AQUA . "...")
+            }
+        }
+    }
   }
 
