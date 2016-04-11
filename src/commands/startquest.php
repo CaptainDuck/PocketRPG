d31d7afd9288d2f4c9325d415e21d811e67fc776
Blabla <st.ten.veldhuis@gmail.com>
2016-04-11 17:59:43 +0200

    Update startquest.php

--- src/commands/startquest.php
+++ src/commands/startquest.php
@@ -4,6 +4,12 @@ namespace Sandertv\Quests
 
 use pocketmine\utils\TextFormat as TF;
 use pocketmine\plugin\PluginBase;
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
 
 class startquest extends PluginBase {
+  
+  public function onCommand(CommandSender $p, Command $cmd, $label, array $args) {
+    
+  }
 
