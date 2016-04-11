51169123383b3f3a01c48b9dd44c78725a0e4de2
Blabla <st.ten.veldhuis@gmail.com>
2016-04-11 16:14:02 +0200

    Update listquest.php

--- src/commands/listquest.php
+++ src/commands/listquest.php
@@ -25,10 +25,36 @@ class listquest extends PluginBase implements Listener {
               $p->sendMessage(TF::RED . "Helping the Farmer - lvl. 0");
           }
 
-          if($p->hasPermission("quest") || $p->hasPermission("quest.2") || $p->hasPermission("quest.2.completed") {
+          if($p->hasPermission("quest") || $p->hasPermission("quest.2") || $p->hasPermission("quest.2.completed") || $p->getExperience() == 1 {
               $p->sendMessage(TF::GREEN . "Not enough Trees - lvl. 1");
           } elseif($p->getExperience()) == 1) {
               $p->sendMessage(TF::YELLOW . "Not enough Trees - lvl. 1");
           } else {
               $p->sendMessage(TF::RED . "Not enough Trees - lvl. 1");
           }
+          
+          if($p->hasPermission("quest") || $p->hasPermission("quest.3") || $p->hasPermission("quest.3.completed") || $p->getExperience() == 2 {
+              $p->sendMessage(TF::GREEN . "Searching the Stick - lvl. 2");
+          } elseif($p->getExperience()) == 2) {
+              $p->sendMessage(TF::YELLOW . "Searching the Stick - lvl. 2");
+          } else {
+              $p->sendMessage(TF::RED . "Searching the Stick - lvl. 2");
+          }
+          
+          if($p->hasPermission("quest") || $p->hasPermission("quest.4") || $p->hasPermission("quest.4.completed") || $p->getExperience() == 3 {
+              $p->sendMessage(TF::GREEN . "Filling the Well - lvl. 3");
+          } elseif($p->getExperience()) == 3) {
+              $p->sendMessage(TF::YELLOW . "Filling the Well - lvl. 3");
+          } else {
+              $p->sendMessage(TF::RED . "Filling the Well - lvl. 3");
+          }
+
+          if($p->hasPermission("quest") || $p->hasPermission("quest.5") || $p->hasPermission("quest.5.completed") || $p->getExperience() == 4 {
+              $p->sendMessage(TF::GREEN . "Growing the Crops - lvl. 4");
+          } elseif($p->getExperience()) == 4) {
+              $p->sendMessage(TF::YELLOW . "Growing the Crops - lvl. 4");
+          } else {
+              $p->sendMessage(TF::RED . "Growing the Crops - lvl. 4");
+          }
+          
+      } elseif(isset)
