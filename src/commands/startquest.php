f58e85b7c4fd2df32c70dda14d050cf115119184
Blabla <st.ten.veldhuis@gmail.com>
2016-04-12 21:36:44 +0200

    Update startquest.php

--- src/commands/startquest.php
+++ src/commands/startquest.php
@@ -14,7 +14,7 @@ class startquest extends PluginBase {
       case "startquest":
         switch(strtolower(array_shift($args))) {
           case "1":
-            if($p->getExpLevel()) == 0) {
+            if($p->getExpLevel()) > -1) {
               $p->sendMessage(TF::GRAY . "Quest started: Helping the Farmer");
               $p->sendMessage(TF::AQUA . "Collect 5 pieces of wheat.");
               $p->givePermission("quest.1.cancomplete");
@@ -24,7 +24,7 @@ class startquest extends PluginBase {
             break;
           
           case "2":
-            if($p->getExpLevel()) == 1) {
+            if($p->getExpLevel()) > 0) {
               $p->sendMessage(TF::GRAY . "Quest started: Not enough Trees");
               $p sendMessage(TF::AQUA . "Collect 16 Oak Saplings")
               $p->givePermission("quest.2.cancomplete");
@@ -34,7 +34,7 @@ class startquest extends PluginBase {
             break;
             
           case "3":
-            if($p->getExpLevel()) == 2) {
+            if($p->getExpLevel()) > 1) {
               $p->sendMessage(TF::GRAY . "Quest started: Searching the Stick");
               $p->sendMessage(TF::AQUA . "Collect 1 stick");
               $p->givePermission("quest.3.cancomplete");
@@ -44,7 +44,7 @@ class startquest extends PluginBase {
             break;
 
           case "4":
-            if($p->getExpLevel()) == 3) {
+            if($p->getExpLevel()) > 2) {
               $p->sendMessage(TF::GRAY . "Quest started: Filling the Well");
               $p->sendMessage(TF::AQUA . "Find a water Bucket");
               $p->givePermission("quest.4.cancomplete");
@@ -54,7 +54,7 @@ class startquest extends PluginBase {
             break;
             
           case "5":
-            if($p->getExpLevel()) == 4) {
+            if($p->getExpLevel()) > 3) {
               $p->sendMessage(TF::GRAY . "Quest started: The lost Gem");
               $p->sendMessage(TF::AQUA . "Collect 1 Emerald");
               $p->givePermission("quest.5.cancomplete");
@@ -64,7 +64,7 @@ class startquest extends PluginBase {
             break;
             
           case "6":
-            if($p->getExpLevel()) == 5) {
+            if($p->getExpLevel()) > 4) {
               $p->sendMessage(TF::GRAY . "Quest started: Freezingly Cold");
               $p->sendMessage(TF::AQUA . "Collect 32 ice blocks");
               $p->givePermission("quest.6.cancomplete");
@@ -74,7 +74,7 @@ class startquest extends PluginBase {
             break;
             
           case "7":
-            if($p->getExpLevel()) == 6) {
+            if($p->getExpLevel()) > 5) {
               $p->sendMessage(TF::GRAY . "Quest started: Gravedigger");
               $p->sendMessage(TF::AQUA . "Collect 5 bones and 5 rotten flesh");
               $p->givePermission("quest.7.cancomplete");
@@ -84,7 +84,7 @@ class startquest extends PluginBase {
             break;
             
           case "8":
-            if($p->getExpLevel()) == 7) {
+            if($p->getExpLevel()) > 6) {
               $p->sendMessage(TF::GRAY . "Quest started: Fish Galore");
               $p->sendMessage(TF::AQUA . "Collect 16 fishes");
               $p->givePermission("quest.8.cancomplete");
@@ -94,7 +94,7 @@ class startquest extends PluginBase {
             break;
             
           case "9":
-            if($p->getExpLevel()) == 8) {
+            if($p->getExpLevel()) > 7) {
               $p->sendMessage(TF::GRAY . "Quest started: Burning Souls");
               $p->sendMessage(TF::AQUA . "Collect 5 blaze powder and 5 blaze rods");
               $p->givePermission("quest.9.cancomplete");
@@ -104,7 +104,7 @@ class startquest extends PluginBase {
             break;
             
           case "10":
-            if($p->getExpLevel()) == 9) {
+            if($p->getExpLevel()) > 8) {
               $p->sendMessage(TF::GRAY . "Quest started: The Experiment");
               $p->sendMessage(TF::AQUA . "Collect 10 glowstone dust and 10 redstone");
               $p->givePermission("quest.10.cancomplete");
