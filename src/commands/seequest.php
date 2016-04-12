8f3a28934e5aa9bf3e2e363c89c79e1799e9d79b
Blabla <st.ten.veldhuis@gmail.com>
2016-04-12 08:35:06 +0200

    Update seequest.php

--- src/commands/seequest.php
+++ src/commands/seequest.php
@@ -7,3 +7,17 @@ use pocketmine\command\Command;
 use pocketmine\command\CommandSender;
 
 class seequest extends PluginBase {
+  
+  public function onCommand(Command $cmd, CommandSender $p, $label, array $args) {
+    switch($cmd->getName()) {
+      case "seequest":
+        switch(strtolower(array_shift($args))) {
+          case "1":
+            if($p hasPermission("quest.1.completed")) {
+              $p->sendMessage(TF:: GREEN . "")
+            }
+        }
+    }
+  }
+
+}
