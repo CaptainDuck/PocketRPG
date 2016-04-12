d9a6fa1cdafabe876157e7264becf74def5c774f
Blabla <st.ten.veldhuis@gmail.com>
2016-04-12 10:14:43 +0200

    Update seequest.php

--- src/commands/seequest.php
+++ src/commands/seequest.php
@@ -13,11 +13,20 @@ class seequest extends PluginBase {
       case "seequest":
         switch(strtolower(array_shift($args))) {
           case "1":
-            if($p hasPermission("quest.1.completed")) {
-              $p->sendMessage(TF:: GREEN . "")
+            if($p->hasPermission("quest.1.completed")) {
+              $p->sendMessage(TF:: GRAY . "Quest 1 - Helping the Farmer "TF:: GREEN . "- Completed!");
+              $p->sendMessage(TF:: AQUA . "Collect 5 pieces of wheat.");
+              $p->sendMessage(TF:: GREEN . "lvl. 0");
+            } elseif($p->hasPermission("quest.1.cancomplete") || $p->getExpLevel() > 0) {
+              $p->sendMessage(TF:: GRAY . "Quest 1 - Helping the Farmer "TF:: YELLOW . "- Uncompleted!");
+              $p->sendMessage(TF:: AQUA . "Collect 5 pieces of wheat.");
+              $p->sendMessage(TF:: YELLOW . "lvl. 0");
+            } else {
+              $p->sendMessage(TF:: RED . "You have not started this quest yet.");
             }
+        
         }
     }
-  }
 
+  }
 }
