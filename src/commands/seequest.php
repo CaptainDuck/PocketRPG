a39bfe7fb6e1fd32b320a799b9c88e80a95d07d9
Blabla <st.ten.veldhuis@gmail.com>
2016-04-12 08:28:16 +0200

    Create seequest.php

---
+++ src/commands/seequest.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Sandertv\Quests
+
+use pocketmine\utils\TextFormat as TF;
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+
+class seequest extends PluginBase {
