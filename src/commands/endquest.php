112e7e6d7aede7ceff2d57b64d4c4f10d27a3234
Blabla <st.ten.veldhuis@gmail.com>
2016-04-13 10:17:52 +0200

    Update endquest.php

--- src/commands/endquest.php
+++ src/commands/endquest.php
@@ -3,10 +3,17 @@
 namespace Sandertv\Quests
 
 use pocketmine\utils\Config;
-use pocketmine\utils\textFormat;
+use pocketmine\utils\TextFormat as TF;
 use pocketmine\command\Command;
 use pocketmine\command\CommandSender;
 
 class endquest extends PluginBase {
 
   public function onCommand(Command $cmd, CommandSender $p, $label, array $args) {
+    switch($cmd->getName()) {
+      case "endquest":
+        switch(strtolower(array_shift($args))) {
+          case: "1":
+            if($p->hasPermission("quest.1.cancomplete") || $p->) 
+        }
+    }
