180bfdcc4d2ff3884be82ac4247bcdf220d10f43
Blabla <st.ten.veldhuis@gmail.com>
2016-04-14 08:18:45 +0200

    Update main.php

--- src/Quests/main/main.php
+++ src/Quests/main/main.php
@@ -1,1 +1,24 @@
 
+<?php
+
+namespace Sandertv\Quests;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\utils\Config;
+
+class main extends PluginBase {
+  
+  public function onLoad() {
+    $this->getLogger()->info("Quests loading!");
+  }
+  
+  public function onEnable() {
+    $this->getLogger()->info("Enabling Quests!");
+      @mkdir($this->getDataFolder());
+      $this->config = new Config ($this->getDataFolder() . "config.yml" , Config::YAML, array());
+  }
+  
+  public function onDisable() {
+    $this->getLogger()->info("Disabling Quests!");
+  }
+}
