d338e422f44c3f328ec18ccffed369aa0f53a244
Blabla <st.ten.veldhuis@gmail.com>
2016-04-17 17:37:03 +0200

    stuff

--- src/RPG/neededstuff/magewand.php
+++ src/RPG/neededstuff/magewand.php
@@ -1,7 +1,24 @@
 <?php
 
-namespace RPG/neededstuff
+namespace RPG/neededstuff;
 
-use pocketmine/plugin/PluginBase
-use pocketmine/event/Listener
-use pocketmine/event/
+use pocketmine/plugin/PluginBase;
+use pocketmine/event/Listener;
+use pocketmine/event/player/PlayerInteractEvent;
+use pocketmine/event/entity/EntityDamageByEntityEvent;
+use pocketmine/entity/Living;
+use pocketmine/math/Vector3;
+use pocketmine/Player;
+use pocketmine/Server;
+
+class magewand extends PluginBase implements Listener {
+  
+  public function onPlayerInteract(PlayerInteractEvent $interactevent) {
+    $p = $interactevent->getPlayer();
+    $configlevel = $this->config->get("RPG_LEVEL");
+    if($p->getLevel() = $configlevel) {
+      if($p->getItemInHand()->getId() = 280) {
+      }
+    }
+  }
+}
