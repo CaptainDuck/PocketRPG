b0f818c2eb01c83d6cdddcf7d531cd4e82770696
Blabla <st.ten.veldhuis@gmail.com>
2016-04-18 09:52:17 +0200

    Update magewand.php

--- src/RPG/neededstuff/magewand.php
+++ src/RPG/neededstuff/magewand.php
@@ -16,8 +16,22 @@ class magewand extends PluginBase implements Listener {
   public function onPlayerInteract(PlayerInteractEvent $interactevent) {
     $p = $interactevent->getPlayer();
     $configlevel = $this->config->get("RPG_LEVEL");
-    if($p->getLevel() = $configlevel) {
-      if($p->getItemInHand()->getId() = 280) {
+    if($p->getLevel() == $configlevel) {
+      if($p->getItemInHand()->getId() == 280) {
+        $b = $interactevent->getBlock();
+        $bx = $b->x;
+        $by = $b->y;
+        $bz = $b->z;
+        $px = $p->x;
+        $py = $p->y;
+        $pz = $p->z;
+        $plocation = $p->getPosition(new Vector3($x, $y, $z));
+        $blocation = $blocktouched->getPosition(new Vector3($x, $y, $z));
+        
+        /////////////////////////////////////////////////////////////////
+        
+        $b->getLocation()
+        
       }
     }
   }
