c6151927f235e3a4760177a70923894f57489ce0
Blabla <st.ten.veldhuis@gmail.com>
2016-04-18 16:42:12 +0200

    Update magewand.php

--- src/RPG/neededstuff/magewand.php
+++ src/RPG/neededstuff/magewand.php
@@ -9,6 +9,7 @@ use pocketmine/event/entity/EntityDamageEvent;
 use pocketmine/entity/Living;
 use pocketmine/math/Vector3;
 use pocketmine/level/Position;
+use pocketmine/level/particle/LavaParticle;
 use pocketmine/Player;
 use pocketmine/Server;
 
@@ -21,8 +22,12 @@ class magewand extends PluginBase implements Listener {
         $level = $this->config->get("RPG_LEVEL");
         if($damager->getLevel() == $configlevel) {
           if($damager->getItemInHand()->getId() == 280) {
+            $x = $hit->x;
+            $y = $hit->y;
+            $z = $hit->z;
             $damagerpos = $damager->getPosition(new Vector3($x, $y, $z));
-            new 
+            $level->addParticle(new LavaParticle($damagerpos));
+            $this->$damager->setKnockBack(2);
           }
         }
       }
