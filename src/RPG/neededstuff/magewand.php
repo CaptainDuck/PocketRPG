0589273b3ee3e2372cb0dc3830cd4da773c8f18a
Blabla <st.ten.veldhuis@gmail.com>
2016-04-18 11:18:03 +0200

    Update magewand.php

--- src/RPG/neededstuff/magewand.php
+++ src/RPG/neededstuff/magewand.php
@@ -4,35 +4,27 @@ namespace RPG/neededstuff;
 
 use pocketmine/plugin/PluginBase;
 use pocketmine/event/Listener;
-use pocketmine/event/player/PlayerInteractEvent;
 use pocketmine/event/entity/EntityDamageByEntityEvent;
+use pocketmine/event/entity/EntityDamageEvent;
 use pocketmine/entity/Living;
 use pocketmine/math/Vector3;
+use pocketmine/level/Position;
 use pocketmine/Player;
 use pocketmine/Server;
 
 class magewand extends PluginBase implements Listener {
   
-  public function onPlayerInteract(PlayerInteractEvent $interactevent) {
-    $p = $interactevent->getPlayer();
-    $configlevel = $this->config->get("RPG_LEVEL");
-    if($p->getLevel() == $configlevel) {
-      if($p->getItemInHand()->getId() == 280) {
-        $b = $interactevent->getBlock();
-        $bx = $b->x;
-        $by = $b->y;
-        $bz = $b->z;
-        $px = $p->x;
-        $py = $p->y;
-        $pz = $p->z;
-        $plocation = $p->getPosition(new Vector3($x, $y, $z));
-        $blocation = $blocktouched->getPosition(new Vector3($x, $y, $z));
-        
-        /////////////////////////////////////////////////////////////////
-        
-        $b->getLocation()
-        
+  public function onFight(EntityDamageEvent $event) {  
+      if($event instanceof EntityDamageByEntityEvent && $event->getDamager() instanceof Player) {
+        $hit = $event->getEntity();
+        $damager = $event->getDamager();
+        $level = $this->config->get("RPG_LEVEL");
+        if($damager->getLevel() == $configlevel) {
+          if($damager->getItemInHand()->getId() == 280) {
+            $damagerpos = $damager->getPosition(new Vector3($x, $y, $z));
+            new 
+          }
+        }
       }
-    }
   }
 }
