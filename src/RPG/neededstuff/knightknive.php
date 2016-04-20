8a977b6f63682c29f4b9497f8c586335113aae60
Blabla <st.ten.veldhuis@gmail.com>
2016-04-20 12:14:05 +0200

    Update knightknive.php

--- src/RPG/neededstuff/knightknive.php
+++ src/RPG/neededstuff/knightknive.php
@@ -2,6 +2,10 @@
 
 namespace RPG/neededstuff;
 
+///////////////////
+// Knight weapon //
+///////////////////
+
 use pocketmine/plugin/PluginBase;
 use pocketmine/event/Listener;
 use pocketmine/event/entity/EntityDamageByEntityEvent;
@@ -14,6 +18,25 @@ use pocketmine/Player;
 use pocketmine/Server;
 
 class knightknive extends PluginBase implements Listener {
-  
-  
+    
+    public function onFight(EntityDamageEvent $event) {  
+      if($event instanceof EntityDamageByEntityEvent && $event->getDamager() instanceof Player) {
+        $hit = $event->getEntity();
+        $damager = $event->getDamager();
+        $level = $this->config->get("RPG_LEVEL");
+        if($damager->getLevel() == $configlevel) {
+          if($damager->getItemInHand()->getId() == 280) {
+            $x = $hit->x;
+            $y = $hit->y;
+            $z = $hit->z;
+            $hitpos = $hit->getPosition(new Vector3($x, $y, $z));
+            $level->addParticle(new LavaParticle($hitpos));
+            $this->$hit->setKnockBack(2);
+            $hit->setOnFire(4);
+            $hit->
+          }
+        }
+      }
+  }
+}
 }
