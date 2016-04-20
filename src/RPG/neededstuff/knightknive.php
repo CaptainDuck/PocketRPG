7c3cec44e466b2255b52c010c91b8c181092565f
Blabla <st.ten.veldhuis@gmail.com>
2016-04-20 12:10:22 +0200

    Update knightknive.php

--- src/RPG/neededstuff/knightknive.php
+++ src/RPG/neededstuff/knightknive.php
@@ -1,3 +1,15 @@
 <?php
 
-namespace
+namespace RPG/neededstuff;
+
+use pocketmine/plugin/PluginBase;
+use pocketmine/Player;
+use pocketmine/Server;
+use pocketmine/event/Listener;
+use pocketmine/event/EntityDamageByEntityEvent;
+use pocketmine/event/EntityDamageEvent;
+
+class knightknive extends PluginBase implements Listener {
+  
+  
+}
