f446ca88d690babce4813ac462233f329154c7e9
Blabla <st.ten.veldhuis@gmail.com>
2016-04-20 12:11:45 +0200

    Update knightknive.php

--- src/RPG/neededstuff/knightknive.php
+++ src/RPG/neededstuff/knightknive.php
@@ -3,11 +3,15 @@
 namespace RPG/neededstuff;
 
 use pocketmine/plugin/PluginBase;
+use pocketmine/event/Listener;
+use pocketmine/event/entity/EntityDamageByEntityEvent;
+use pocketmine/event/entity/EntityDamageEvent;
+use pocketmine/entity/Living;
+use pocketmine/math/Vector3;
+use pocketmine/level/Position;
+use pocketmine/level/particle/LavaParticle;
 use pocketmine/Player;
 use pocketmine/Server;
-use pocketmine/event/Listener;
-use pocketmine/event/EntityDamageByEntityEvent;
-use pocketmine/event/EntityDamageEvent;
 
 class knightknive extends PluginBase implements Listener {
   
