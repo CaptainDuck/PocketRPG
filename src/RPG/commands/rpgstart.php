7b3f73b9fbf34e886c638048f50b0b22e276e24f
Blabla <st.ten.veldhuis@gmail.com>
2016-04-22 14:02:02 +0200

    added classes to start...

--- src/RPG/commands/rpgstart.php
+++ src/RPG/commands/rpgstart.php
@@ -11,10 +11,52 @@ use pocketmine/Player;
 class rpgstart extends PluginBase {
   
   public function onCommand(Command $cmd, CommandSender $p, $label, array $args) {
-    switch ($cmd->getName) {
+    switch($cmd->getName) {
       case "rpgstart":
-        switch () {
-          
+        switch(strtolower(array_shift($args))) {
+          case "mage":
+            if($p->hasPermission("class.chosen")) {
+              $p->sendMessage(TF:: RED . "You have already picked a class!")
+            } else {
+              $p->sendMessage(TF:: AQUA . "You have joined the world as a mage!");
+              $wand = Item::get(Item::STICK, 0, 1);
+              $p->getInventory->addItem($wand);
+              $p->givePermission("class.chosen");
+            }
+            break;
+            
+          case "warrior":
+            if($p->hasPermission("class.chosen")) {
+              $p->sendMessage(TF:: RED . "You have already picked a class!")
+            } else {
+              $p->sendMessage(TF:: AQUA . "You have joined the world as a warrior!");
+              $sword = Item::get(Item::IRON_SWORD, 0, 1);
+              $p->getInventory->addItem($sword);
+              $p->givePermission("class.chosen");
+            }
+            break;
+            
+          case "tanker":
+            if($p->hasPermission("class.chosen")) {
+              $p->sendMessage(TF:: RED . "You have already picked a class!")
+            } else {
+              $p->sendMessage(TF:: AQUA . "You have joined the world as a tanker!");
+              $shield = Item::get(Item::MINECART, 0, 1);
+              $p->getInventory->addItem($shield);
+              $p->givePermission("class.chosen");
+            }
+            break;
+            
+          case "assassin":
+            if($p->hasPermission("class.chosen")) {
+              $p->sendMessage(TF:: RED . "You have already picked a class!")
+            } else {
+              $p->sendMessage(TF:: AQUA . "You have joined the world as an assassin!");
+              $knive = Item::get(Item::FEATHER, 0, 1);
+              $p->getInventory->addItem($knive);
+              $p->givePermission("class.chosen");
+            }
+            break;
         }
     }
   }
