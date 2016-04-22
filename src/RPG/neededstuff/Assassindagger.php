6d99a59b3897381c69d8b3c5a6e1d7ca666540e7
Blabla <st.ten.veldhuis@gmail.com>
2016-04-22 11:12:19 +0200

    Update Assassindagger.php

--- src/RPG/neededstuff/Assassindagger.php
+++ src/RPG/neededstuff/Assassindagger.php
@@ -2,4 +2,45 @@
 
 namespace Sandertv/PocketRPG;
 
+/////////////////////
+// Assassin Dagger //
+/////////////////////
+
 use pocketmine/plugin/PluginBase;
+use pocketmine/event/Listener;
+use pocketmine/event/entity/EntityDamageByEntityEvent;
+use pocketmine/event/entity/EntityDamageEvent;
+use pocketmine/event/player/PlayerItemHeldEvent;
+use pocketmine/entity/Living;
+use pocketmine/math/Vector3;
+use pocketmine/level/Position;
+use pocketmine/level/particle/CriticalParticle;
+use pocketmine/Player;
+use pocketmine/Server;
+class knightknive extends PluginBase implements Listener {
+    
+    public function onFight(EntityDamageEvent $event) {  
+      if($event instanceof EntityDamageByEntityEvent && $event->getDamager() instanceof Player) {
+        $hit = $event->getEntity();
+        $damager = $event->getDamager();
+        $level = $this->config->get("RPG_LEVEL");
+        if($damager->getLevel() == $configlevel) {
+          if($damager->getItemInHand()->getId() == 388) {
+            $x = $hit->x;
+            $y = $hit->y;
+            $z = $hit->z;
+            $hitpos = $hit->getPosition(new Vector3($x, $y, $z));
+            $level->addParticle(new CriticalParticle($hitpos));
+            $this->setDamage(getDamage() + 3);
+          }
+        }
+      }
+  }
+    public function onItemHeld(PlayerItemHeldEvent $event2) {
+        $p = $event2->getPlayer();
+        if($p->getItemInHand()->getId() == 347) {
+            $effect = Effect::getEffect(14)->setDuration(5)->setAmplifier(1)->setVisible(true);
+            $p->addEffect($effect);
+        }
+    }
+}
