31c074fd43e01d9f628f3cb0ab01b84e50e7a26c
Blabla <st.ten.veldhuis@gmail.com>
2016-04-22 11:18:15 +0200

    Update rpgstart.php

--- src/RPG/commands/rpgstart.php
+++ src/RPG/commands/rpgstart.php
@@ -1,1 +1,21 @@
+<?php
 
+namespace Sandertv/PocketRPG;
+
+use pocketmine/plugin/PluginBase;
+use pocketmine/command/Command;
+use pocketmine/command/CommandSender;
+use pocketmine/Server;
+use pocketmine/Player;
+
+class rpgstart extends PluginBase {
+  
+  public function onCommand(Command $cmd, CommandSender $p, $label, array $args) {
+    switch ($cmd->getName) {
+      case "rpgstart":
+        switch () {
+          
+        }
+    }
+  }
+}
