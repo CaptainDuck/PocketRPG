3662be5084a595d10d7c859fa85ab107aedf4a76
Blabla <st.ten.veldhuis@gmail.com>
2016-04-23 16:39:32 +0200

    Update magewand.php

--- src/RPG/neededstuff/magewand.php
+++ src/RPG/neededstuff/magewand.php
@@ -1,21 +1,21 @@
 <?php
 
-namespace Sandertv/PocketRPG;
+namespace Sandertv\PocketRPG;
 
 /////////////////
 // Mage weapon //
 /////////////////
 
-use pocketmine/plugin/PluginBase;
-use pocketmine/event/Listener;
-use pocketmine/event/entity/EntityDamageByEntityEvent;
-use pocketmine/event/entity/EntityDamageEvent;
-use pocketmine/entity/Living;
-use pocketmine/math/Vector3;
-use pocketmine/level/Position;
-use pocketmine/level/particle/LavaParticle;
-use pocketmine/Player;
-use pocketmine/Server;
+use pocketmine\plugin\PluginBase;
+use pocketmine\event\Listener;
+use pocketmine\event\entity\EntityDamageByEntityEvent;
+use pocketmine\event\entity\EntityDamageEvent;
+use pocketmine\entity\Living;
+use pocketmine\math\Vector3;
+use pocketmine\level\Position;
+use pocketmine\level\particle\LavaParticle;
+use pocketmine\Player;
+use pocketmine\Server;
 
 class magewand extends PluginBase implements Listener {
   
