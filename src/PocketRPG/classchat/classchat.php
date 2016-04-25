a6db4e23793928747c2872a07fb6f6ae664b4509
Blabla <st.ten.veldhuis@gmail.com>
2016-04-25 21:14:06 +0200

    Update classchat.php

--- src/PocketRPG/classchat/classchat.php
+++ src/PocketRPG/classchat/classchat.php
@@ -4,10 +4,18 @@ namespace PocketRPG/classchat;
 
 use pocketmine/plugin/PluginBase;
 use pocketmine/utils/TextFormat as TF;
+use pocketmine/event/Listener;
+use pocketmine/event/PlayerChatEvent;
 use pocketmine/Server;
 use pocketmine/Player;
 
-class classchat extends PluginBase {
+class classchat extends PluginBase implements Listener {
   
-  
+  public function onChat(PlayerChatEvent $event) {
+    $p = $event->getPlayer();
+    if($p->hasPermission("class.mage")) {
+      $this->getServer()->broadcastMessage("<$player->getName()> Mage > $event->getMessage()");
+      $event->setCancelled();
+    } elseif($p->hasPermission("class.warrior"))
+  }
 }
