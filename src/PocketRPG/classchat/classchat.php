a2a7d582595ed1b4f8115ae33cf8c4e695ad0729
Blabla <st.ten.veldhuis@gmail.com>
2016-04-25 15:33:50 +0200

    Create classchat.php

---
+++ src/PocketRPG/classchat/classchat.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace PocketRPG/classchat;
+
+use pocketmine/plugin/PluginBase;
+use pocketmine/utils/TextFormat as TF;
+use pocketmine/Server;
+use pocketmine/Player;
+
+class classchat extends PluginBase
