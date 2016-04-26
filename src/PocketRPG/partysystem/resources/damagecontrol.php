518facd1a8f008ef1317bfa2b37d136bd4c5aa63
Blabla <st.ten.veldhuis@gmail.com>
2016-04-26 15:20:08 +0200

    Create damagecontrol.php

---
+++ src/PocketRPG/partysystem/resources/damagecontrol.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace PocketRPG/partysystem/resources;
+
+use pocketmine/plugin/PluginBase;
+use pocketmine/event/Listener;
+use pocketmine/event/EntityDamageEvent;
+use pocketmine/event/EntityDamageByEntityEvent;
+
+class damagecontrol extends PluginBase implements Listener {
+  
+}
