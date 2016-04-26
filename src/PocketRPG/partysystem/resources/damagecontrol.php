8441842a8ded03f45c6e3e55c4226d99e8334158
Blabla <st.ten.veldhuis@gmail.com>
2016-04-26 15:24:30 +0200

    Update damagecontrol.php

--- src/PocketRPG/partysystem/resources/damagecontrol.php
+++ src/PocketRPG/partysystem/resources/damagecontrol.php
@@ -1,11 +1,12 @@
 <?php
 
-namespace PocketRPG/partysystem/resources;
+namespace PocketRPG\partysystem\resources;
 
-use pocketmine/plugin/PluginBase;
-use pocketmine/event/Listener;
-use pocketmine/event/EntityDamageEvent;
-use pocketmine/event/EntityDamageByEntityEvent;
+use pocketmine\plugin\PluginBase;
+use pocketmine\event\Listener;
+use pocketmine\event\EntityDamageEvent;
+use pocketmine\event\EntityDamageByEntityEvent;
+use pocketmine\utils\Config;
 
 class damagecontrol extends PluginBase implements Listener {
   
