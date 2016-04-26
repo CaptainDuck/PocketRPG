dc3c0e250af0925cd8a2da066e1d1d9258fac946
Blabla <st.ten.veldhuis@gmail.com>
2016-04-26 13:01:28 +0200

    Update startquest.php

--- src/PocketRPG/Quests/commands/startquest.php
+++ src/PocketRPG/Quests/commands/startquest.php
@@ -19,7 +19,7 @@ class startquest extends PluginBase {
             if($p->getExpLevel() > -1) {
               $p->sendMessage(TF::GRAY . "Quest started: Helping the Farmer");
               $p->sendMessage(TF::AQUA . "Collect 5 pieces of wheat.");
-              $p->givePermission("quest.1.cancomplete");
+              $p->setPermission("quest.1.cancomplete");
             } elseif($p->hasPermission("quest.1.cancomplete")) {
               $p->sendMessage(TF:: RED . "You already started this quest!");
             } else {
@@ -31,7 +31,7 @@ class startquest extends PluginBase {
             if($p->getExpLevel() > 0) {
               $p->sendMessage(TF::GRAY . "Quest started: Not enough Trees");
               $p->sendMessage(TF::AQUA . "Collect 16 Oak Saplings");
-              $p->givePermission("quest.2.cancomplete");
+              $p->setPermission("quest.2.cancomplete");
             } elseif($p->hasPermission("quest.2.cancomplete")) {
               $p->sendMessage(TF:: RED . "You already started this quest!");
             } else {
@@ -43,7 +43,7 @@ class startquest extends PluginBase {
             if($p->getExpLevel() > 1) {
               $p->sendMessage(TF::GRAY . "Quest started: Searching the Stick");
               $p->sendMessage(TF::AQUA . "Collect 1 stick");
-              $p->givePermission("quest.3.cancomplete");
+              $p->setPermission("quest.3.cancomplete");
             } elseif($p->hasPermission("quest.3.cancomplete")) {
               $p->sendMessage(TF:: RED . "You already started this quest!");
             } else {
@@ -55,7 +55,7 @@ class startquest extends PluginBase {
             if($p->getExpLevel() > 2) {
               $p->sendMessage(TF::GRAY . "Quest started: Shearing the Sheap");
               $p->sendMessage(TF::AQUA . "Collect 1 shears");
-              $p->givePermission("quest.4.cancomplete");
+              $p->setPermission("quest.4.cancomplete");
             } elseif($p->hasPermission("quest.4.cancomplete")) {
               $p->sendMessage(TF:: RED . "You already started this quest!");
             } else {
@@ -67,7 +67,7 @@ class startquest extends PluginBase {
             if($p->getExpLevel() > 3) {
               $p->sendMessage(TF::GRAY . "Quest started: The lost Gem");
               $p->sendMessage(TF::AQUA . "Collect 1 Emerald");
-              $p->givePermission("quest.5.cancomplete");
+              $p->setPermission("quest.5.cancomplete");
             } elseif($p->hasPermission("quest.5.cancomplete")) {
               $p->sendMessage(TF:: RED . "You already started this quest!");
             } else {
@@ -79,7 +79,7 @@ class startquest extends PluginBase {
             if($p->getExpLevel() > 4) {
               $p->sendMessage(TF::GRAY . "Quest started: Freezingly Cold");
               $p->sendMessage(TF::AQUA . "Collect 32 ice blocks");
-              $p->givePermission("quest.6.cancomplete");
+              $p->setPermission("quest.6.cancomplete");
             } elseif($p->hasPermission("quest.6.cancomplete")) {
               $p->sendMessage(TF:: RED . "You already started this quest!");
             } else {
@@ -91,7 +91,7 @@ class startquest extends PluginBase {
             if($p->getExpLevel() > 5) {
               $p->sendMessage(TF::GRAY . "Quest started: Gravedigger");
               $p->sendMessage(TF::AQUA . "Collect 5 bones and 5 rotten flesh");
-              $p->givePermission("quest.7.cancomplete");
+              $p->setPermission("quest.7.cancomplete");
             } else {
               $p->sendMessage(TF:: RED . "You are not the right level to start this quest.");
             } elseif($p->hasPermission("quest.7.cancomplete")) {
@@ -103,7 +103,7 @@ class startquest extends PluginBase {
             if($p->getExpLevel() > 6) {
               $p->sendMessage(TF::GRAY . "Quest started: Fish Galore");
               $p->sendMessage(TF::AQUA . "Collect 16 fishes");
-              $p->givePermission("quest.8.cancomplete");
+              $p->setPermission("quest.8.cancomplete");
             } elseif($p->hasPermission("quest.8.cancomplete")) {
               $p->sendMessage(TF:: RED . "You already started this quest!");
             } else {
@@ -115,7 +115,7 @@ class startquest extends PluginBase {
             if($p->getExpLevel() > 7) {
               $p->sendMessage(TF::GRAY . "Quest started: Burning Souls");
               $p->sendMessage(TF::AQUA . "Collect 5 blaze powder and 5 blaze rods");
-              $p->givePermission("quest.9.cancomplete");
+              $p->setPermission("quest.9.cancomplete");
             } elseif($p->hasPermission("quest.9.cancomplete")) {
               $p->sendMessage(TF:: RED . "You already started this quest!");
             } else {
@@ -127,7 +127,7 @@ class startquest extends PluginBase {
             if($p->getExpLevel() > 8) {
               $p->sendMessage(TF::GRAY . "Quest started: The Experiment");
               $p->sendMessage(TF::AQUA . "Collect 10 glowstone dust and 10 redstone");
-              $p->givePermission("quest.10.cancomplete");
+              $p->setPermission("quest.10.cancomplete");
             } elseif($p->hasPermission("quest.10.cancomplete")) {
               $p->sendMessage(TF:: RED . "You already started this quest!");
             } else {
