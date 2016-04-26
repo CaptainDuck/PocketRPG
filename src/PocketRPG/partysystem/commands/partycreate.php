8ff36d2ed3676f96b3dc99e34f9018c0abbe3a24
Blabla <st.ten.veldhuis@gmail.com>
2016-04-26 15:23:28 +0200

    Create partycreate.php

---
+++ src/PocketRPG/partysystem/commands/partycreate.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace PocketRPG\partysystem\commands;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\utils\TextFormat as TF;
+use pocketmine\utils\config;
+use pocketmine\Command;
+use pocketmine\command\CommandSender;
+
+class partycreate extends PluginBase implements Listener {
+  
+}
