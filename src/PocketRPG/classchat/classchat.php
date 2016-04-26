d6ffb22cdc27c514d406a9588bce2b9c58396458
Blabla <st.ten.veldhuis@gmail.com>
2016-04-26 08:41:11 +0200

    added special chat for all ranks.

--- src/PocketRPG/classchat/classchat.php
+++ src/PocketRPG/classchat/classchat.php
@@ -13,9 +13,24 @@ class classchat extends PluginBase implements Listener {
   
   public function onChat(PlayerChatEvent $event) {
     $p = $event->getPlayer();
-    if($p->hasPermission("class.mage")) {
-      $this->getServer()->broadcastMessage("<$player->getName()> Mage > $event->getMessage()");
-      $event->setCancelled();
-    } elseif($p->hasPermission("class.warrior"))
+    $rpglevel = $this->config->get("RPG_LEVEL");
+    if($p->getLevel() == $rpglevel) {
+      if($p->hasPermission("class.mage")) {
+        $this->getServer()->broadcastMessage("<$player->getName()> Mage > $event->getMessage()");
+        $event->setCancelled();
+      } elseif($p->hasPermission("class.warrior")) {
+        $this->getServer()->broadcastMessage("<$player->getName()> Warrior > $event->getMessage()");
+        $event->setCancelled();
+      } elseif($p->hasPermission("class.tanker")) {
+        $this->getServer()->broadcastMessage("<$player->getName()> Tanker > $event->getMessage()");
+        $event->setCancelled();
+      } elseif($p->hasPermission("class.assassin")) {
+        $this->getServer()->broadcastMessage("<$player->getName()> Assassin > $event->getMessage()");
+        $event->setCancelled();
+      } else {
+        $this->getServer()->broadcastMessage("<$player->getName()> $event->getMessage()");
+        $event->setCancelled();
+      }
+    }
   }
 }
