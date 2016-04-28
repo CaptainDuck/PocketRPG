20dd0d620d0ab136282e65a045952b0304dc81e5
Blabla <st.ten.veldhuis@gmail.com>
2016-04-28 10:01:47 +0200

    Update and rename partycreate.php to createparty.php

--- src/PocketRPG/partysystem/commands/createparty.php
+++ src/PocketRPG/partysystem/commands/createparty.php
@@ -11,6 +11,14 @@ use pocketmine\command\CommandSender;
 class partycreate extends PluginBase implements Listener {
   
   public function onCommand(CommandSender $p, Command $cmd, $label, array $args) {
-    
+    if($cmd->getName()) == "createparty") {
+      if(!isset($args[0])) {
+        $p->sendMessage(TF:: RED . "Please specify your party name.");
+      } else {
+        $partyname = $this->$args[0];
+        $p->sendMessage(TF:: AQUA . "You have started a party named $partyname");
+        $p->setPermission("party.$partyname");
+      }
+    }
   }
 }
