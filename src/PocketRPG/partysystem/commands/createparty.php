d1b99d393f2886b9e11b4e46f1e7c7f4f63e3bc6
Blabla <st.ten.veldhuis@gmail.com>
2016-04-29 10:52:59 +0200

    Update createparty.php

--- src/PocketRPG/partysystem/commands/createparty.php
+++ src/PocketRPG/partysystem/commands/createparty.php
@@ -8,18 +8,18 @@ use pocketmine\utils\config;
 use pocketmine\Command;
 use pocketmine\command\CommandSender;
 
-class partycreate extends PluginBase implements Listener {
+class partycreate extends PluginBase {
   
   public function onCommand(CommandSender $p, Command $cmd, $label, array $args) {
     if($cmd->getName()) == "createparty") {
+      $partyname = $this->$args[0];
       if(!isset($args[0])) {
         $p->sendMessage(TF:: RED . "Please specify your party name.");
-      } elseif($p->hasPermission("party.joined")) {
+      } elseif($p->hasPermission("party."$partyname"joined")) {
         $p->sendMessage(TF:: RED . "You are already in a party!");
       } else {
-        $partyname = $this->$args[0];
         $p->sendMessage(TF:: AQUA . "You have started a party named $partyname!");
-        $p->setPermission("party.joined");
+        $p->setPermission("party."$partyname"joined");
       }
     }
   return true;
