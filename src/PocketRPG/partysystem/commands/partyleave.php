39203a6aaad4cd63c095726987479301d7a14f57
Blabla <st.ten.veldhuis@gmail.com>
2016-05-03 20:46:17 +0200

    Update partyleave.php

--- src/PocketRPG/partysystem/commands/partyleave.php
+++ src/PocketRPG/partysystem/commands/partyleave.php
@@ -12,6 +12,18 @@ use pocketmine\command/CommandSender;
 class partyleave extends PluginBase {
   
   public function onCommand(CommandSender $p, Command $cmd, $label, array $args) {
-    
+    if($cmd->getName() == "partyleave") {
+      $cfglevel = $this->config->get("RPG_LEVEL");
+      if($p->getLevel() == $cfglevel) {
+        if(!isset($args[0])) {
+          $p->sendMessage(TF:: RED . "Usage: /partyleave <partyname>");
+        } elseif($p->hasPermission("party. " . $args[0] . "")) {
+          $p->sendMessage(TF:: GREEN . "You have left the party " . $args[0] . "");
+          $p->unsetPermission("party.joined" & "party." . $args[0] . "");
+        } else {
+          $p->sendMessage(TF:: RED . "You have to join a guild before being able to leave one.");
+        }
+      }
+    }
   }
 }
