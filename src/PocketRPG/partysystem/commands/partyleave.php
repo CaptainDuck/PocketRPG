98103458523303a2b64706a995cc58f694b964d1
Blabla <st.ten.veldhuis@gmail.com>
2016-05-03 20:37:04 +0200

    Create partyleave.php

---
+++ src/PocketRPG/partysystem/commands/partyleave.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace PocketRPG\partysystem\commands;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\Server;
+use pocketmine\Player;
+use pocketmine\utils\TextFormat as TF;
+use pocketmine\command\Command;
+use pocketmine\command/CommandSender;
+
+class partyleave extends PluginBase {
+  
+  public function onCommand(CommandSender $p, Command $cmd, $label, array $args) {
+    
+  }
+}
