2a7094c25c0a855debc4025d131f0b10b09eb9a9
Blabla <st.ten.veldhuis@gmail.com>
2016-05-04 18:00:55 +0200

    Update main.php

--- src/PocketRPG/main.php
+++ src/PocketRPG/main.php
@@ -23,12 +23,6 @@ class main extends PluginBase {
   public function onDisable() {
     $this->getLogger()->info("Disabling PocketRPG!");
   }
-  
-  public function __construct(PocketRPG $plugin, Player $player, Arena $arena){
-    parent::__construct($plugin);
-    $this->player = $player;
-    $this->arena = $arena;
-    }
     
   public function getPlayer(){
     return $this->player;
