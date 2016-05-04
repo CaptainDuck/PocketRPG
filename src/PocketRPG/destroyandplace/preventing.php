e44244e48452d30c7f62a1fc6abdcb2b9dc0d743
Blabla <st.ten.veldhuis@gmail.com>
2016-05-04 08:57:26 +0200

    Update preventing.php

--- src/PocketRPG/destroyandplace/preventing.php
+++ src/PocketRPG/destroyandplace/preventing.php
@@ -1,3 +1,33 @@
 <?php
 
 use pocketmine\plugin\PluginBase;
+use pocketmine\Player;
+use pocketmine\Server;
+use pocketmine\event\Listener;
+use pocketmine\event\BlockBreakEvent;
+use pocketmine\event\BlockPlaceEvent;
+use pocketmine\utils\TextFormat as TF;
+use pocketmine\utils\Config;
+
+class preventing extends PluginBase implements Listener {
+  
+  public function onBreak(BlockBreakEvent $event) {
+    $cfglevel = $this->config->get("RPG_LEVEL");
+    $p = $event->getPlayer();
+    if($p instanceof Player) {
+      if($p->getLevel() == $cfglevel) {
+        $event->setCancelled();
+        $p->sendMessage(TF:: RED . "You are not allowed to do that here.");
+      }
+    }
+  }
+  
+  public function onPlace(BlockPlaceEvent $event2) {
+    if($p instanceof Player) {
+      if($p->getLevel() == $cfglevel) {
+        $event->setCancelled();
+        $p->sendMessage(TF:: RED . "You are not allowed to do that here.")
+      }
+    }
+  }
+}
